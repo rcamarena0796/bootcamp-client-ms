67a28a9ccebb107283b9bcdb7f5799d859d42b0a
Rubén <ruben.camarena@pucp.pe>
2020-03-23 15:55:16 -0500

    eureka server config

--- src/test/java/com/everis/bootcamp/clientms/ClientmsApplicationTests.java
+++ src/test/java/com/everis/bootcamp/clientms/ClientmsApplicationTests.java
@@ -2,7 +2,10 @@ package com.everis.bootcamp.clientms;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+
+@EnableDiscoveryClient
 @SpringBootTest
 class ClientmsApplicationTests {
 
--- src/test/java/com/everis/bootcamp/clientms/expose/ClientmsControllerTest.java
+++ src/test/java/com/everis/bootcamp/clientms/expose/ClientmsControllerTest.java
@@ -1,20 +1,31 @@
 package com.everis.bootcamp.clientms.expose;
 
-
-
+import com.everis.bootcamp.clientms.controller.ClientController;
+import com.everis.bootcamp.clientms.dao.ClientRepository;
 import com.everis.bootcamp.clientms.model.Client;
+import com.everis.bootcamp.clientms.service.ClientService;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+
 @ExtendWith(SpringExtension.class)
-@WebFluxTest
+@WebFluxTest(controllers = ClientController.class)
+@Import(ClientService.class)
 public class ClientmsControllerTest {
+
+    @MockBean
+    protected ClientService clientService;
+
+    @MockBean
+    ClientRepository repository;
 
     @Autowired
     private WebTestClient webClient;
@@ -28,7 +39,7 @@ public class ClientmsControllerTest {
     }
 
     @Test
-    public void test_controller_hola_mundo(){
+    public void test_controller_hola_mundo() {
         webClient.get()
                 .uri("/api/client/test")
                 .accept(MediaType.APPLICATION_JSON)
